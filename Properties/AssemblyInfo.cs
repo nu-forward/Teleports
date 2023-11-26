1467e43cdcaef3cc392cd15323e4447545c4f785
NotAloneAgain <maoffru@gmail.com>
2023-11-26 19:54:53 +0300

    bump

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -2,9 +2,9 @@
 using System.Runtime.InteropServices;
 
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("5.0.0")]
+[assembly: AssemblyVersion("5.0.2")]
 [assembly: AssemblyTitle("Teleports")]
-[assembly: AssemblyFileVersion("5.0.0")]
+[assembly: AssemblyFileVersion("5.0.2")]
 [assembly: AssemblyProduct("Teleports")]
 [assembly: Guid("1ddaf82d-6a89-4c9d-9b1f-adf4e4fbe8c3")]
 [assembly: AssemblyCompany("Timersky & NotAloneAgain")]
